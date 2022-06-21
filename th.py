ff8f3e964726971262a083040ac2ce37cbf6aa4a
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-21 08:57:24 +0200

    failed to add pip well

--- th.py
+++ th.py
@@ -1,1 +1,8 @@
-print("x")
+command = input("Enter command: ")
+while command != "quit":
+    if command == "start":
+        print("Car started")
+    elif command == "stop":
+        print("Car stopped")
+    elif command == "help":
+        print("start - to start the car\nstop - to stop the car\nquit - to quit")
