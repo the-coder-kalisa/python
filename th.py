a34b19be98b1ff703888bb9de337e61923075c26
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-21 09:05:34 +0200

    starting car

--- th.py
+++ th.py
@@ -1,8 +1,23 @@
-command = input("Enter command: ")
-while command != "quit":
+command = ""
+started = False
+while True:
+    command = input("> ").lower()
     if command == "start":
-        print("Car started")
+        if started:
+            print("Car is already started")
+        else:
+            started = True
+            print("Car started")
     elif command == "stop":
-        print("Car stopped")
+        if not started:
+            print("Car is already stopped")
+        else:
+            started = False
+            print("Car stopped")
+    elif command == "quit":
+        print("Bye")
+        break
     elif command == "help":
         print("start - to start the car\nstop - to stop the car\nquit - to quit")
+    else:
+        print("Sorry, I don't understand that command. Please use help to see all commands")
