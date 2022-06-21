f05021806623d7e2cb93a29ddbc5b7c4c3de1a73
the-coder-kalisa <gthecoderkalisaineza@gmail.com>
2022-06-21 09:09:45 +0200

    writing F

--- th.py
+++ th.py
@@ -1,23 +1,6 @@
-command = ""
-started = False
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        if started:
-            print("Car is already started")
-        else:
-            started = True
-            print("Car started")
-    elif command == "stop":
-        if not started:
-            print("Car is already stopped")
-        else:
-            started = False
-            print("Car stopped")
-    elif command == "quit":
-        print("Bye")
-        break
-    elif command == "help":
-        print("start - to start the car\nstop - to stop the car\nquit - to quit")
-    else:
-        print("Sorry, I don't understand that command. Please use help to see all commands")
+numbers = [5, 2, 5, 2, 2]
+for number in numbers:
+    output = ""
+    for i in range(number) :
+        output += "*"
+    print(output)
